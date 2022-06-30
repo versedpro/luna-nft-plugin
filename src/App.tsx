7755eb592b97371030162601a2b7809c21af1310
Sportigos <sportigo1120@gmail.com>
2022-06-30 12:25:09 -0500

    feat: ui updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import { Widget } from "@luna-market-deployer/nft-checkout";
-// import { Widget } from "./lib";
-import "./App.css";
+import React from 'react';
+// import { Widget } from "@luna-market-deployer/nft-checkout";
+import { Widget } from './lib';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Widget
-        bgimg="/assets/background.png"
-        curryLogo="/assets/curry-logo.png"
-        lunaLogo="/assets/luna-logo.svg"
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Widget
+                bgimg="/assets/background.png"
+                curryLogo="/assets/curry-logo.png"
+                lunaLogo="/assets/luna-logo.svg"
+                collectionTitle="Curry Brand - Into the Metaverse"
+                nftTitle="Into The Metaverse"
+                nftDescription="This is a short description of the “Into the Metaverse” NFT project. This is a short description of the “Into the Metaverse” NFT project. "
+                price={0.08}
+                mintsRemain={10000}
+                mintBtnDisabled={true}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/lib/components/Widget/index.tsx
+++ src/lib/components/Widget/index.tsx
@@ -1,152 +1,147 @@
-import React from "react";
-// import './style.css';
+import React from 'react';
+import './style.css';
 
 type ComponentProps = {
-  bgimg?: string;
-  curryLogo?: string;
-  lunaLogo?: string;
-  onConnectWallet?: () => void;
+    bgimg?: string;
+    curryLogo?: string;
+    lunaLogo?: string;
+    collectionTitle: string;
+    nftTitle: string;
+    nftDescription: string;
+    price: number;
+    mintsRemain: number;
+    mintBtnDisabled: boolean;
+    onConnectWallet?: () => void;
+    onMintNft?: () => void;
 };
 
 const Widget: React.FC<ComponentProps> = ({
-  bgimg,
-  curryLogo,
-  lunaLogo,
-  onConnectWallet,
+    bgimg,
+    curryLogo,
+    lunaLogo,
+    collectionTitle,
+    nftTitle,
+    nftDescription,
+    price,
+    mintsRemain,
+    mintBtnDisabled,
+    onConnectWallet,
+    onMintNft,
 }): JSX.Element => {
-  return (
-    <div
-      style={{
-        width: "100%",
-        minHeight: 240,
-        maxHeight: 380,
-        boxSizing: "border-box",
-        display: "flex",
-        flexDirection: "row",
-        textAlign: "left",
-        padding: 8,
-        background: "#1d1d1d",
-      }}
-    >
-      <div style={{ width: "60%", position: "relative" }}>
-        <img
-          src={bgimg}
-          width="100%"
-          height="100%"
-          alt=""
-          style={{ objectFit: "cover" }}
-        />
-        <div style={{ position: "absolute", inset: 0 }}>
-          <div
-            style={{
-              width: "100%",
-              height: "60%",
-              background:
-                "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(27, 28, 34, 0) 0.01%, #000000 100%)",
-              transform: "rotate(-180deg)",
-            }}
-          ></div>
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            inset: 0,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            padding: 8,
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <img src={curryLogo} width={40} height={40} alt="" />
-            <p style={{ fontSize: 20, color: "white", marginLeft: 16 }}>
-              Curry Brand - Into the Metaverse
-            </p>
-          </div>
-          <div
-            style={{
-              width: "fit-content",
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              background: "#000000BF",
-              padding: 8,
-              marginLeft: -8,
-            }}
-          >
-            <p style={{ fontSize: 14, color: "white", margin: 0 }}>
-              Powered by
-            </p>
-            <img
-              src={lunaLogo}
-              width={16}
-              height={16}
-              alt=""
-              style={{ marginLeft: 6 }}
-            />
-          </div>
-        </div>
-      </div>
-      <div
-        style={{
-          width: "40%",
-          display: "flex",
-          flexDirection: "column",
-          position: "relative",
-        }}
-      >
-        <div style={{ padding: 16 }}>
-          <p style={{ fontSize: 20, color: "white" }}>Into the Metaverse</p>
-        </div>
-        <div style={{ height: 1, background: "#444444" }}></div>
-        <div
-          style={{
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            padding: 16,
-          }}
-        >
-          <p style={{ fontSize: 14, color: "#9E9E9E" }}>
-            This is a short description of the “Into the Metaverse” NFT project.
-          </p>
-          <div>
-            <div
-              style={{ display: "flex", flexDirection: "row", color: "white" }}
-            >
-              <div>
-                <p>Price</p>
-                <p style={{ fontWeight: 600 }}>0.08 ETH</p>
-              </div>
-              <div style={{ marginLeft: 60 }}>
-                <p>Mints Remaining</p>
-                <p style={{ fontWeight: 600 }}>20,000</p>
-              </div>
+    return (
+        <div className="container">
+            <div style={{ width: '40%', position: 'relative' }}>
+                <img src={bgimg} width="100%" height="100%" alt="" style={{ objectFit: 'cover' }} />
+                <div style={{ position: 'absolute', inset: 0 }}>
+                    <div
+                        style={{
+                            width: '100%',
+                            height: '60%',
+                            background:
+                                'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(27, 28, 34, 0) 0.01%, #000000 100%)',
+                            transform: 'rotate(-180deg)',
+                        }}
+                    ></div>
+                </div>
+                <div
+                    style={{
+                        position: 'absolute',
+                        inset: 0,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                        padding: 8,
+                    }}
+                >
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <img src={curryLogo} width={40} height={40} alt="" />
+                            <p style={{ fontSize: 20, color: 'white', marginLeft: 16 }}>{collectionTitle}</p>
+                        </div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                            }}
+                        ></div>
+                    </div>
+                    <div
+                        style={{
+                            width: 'fit-content',
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            background: '#000000BF',
+                            padding: 8,
+                            marginLeft: -8,
+                        }}
+                    >
+                        <p style={{ fontSize: 14, color: 'white', margin: 0 }}>Powered by</p>
+                        <img src={lunaLogo} width={16} height={16} alt="" style={{ marginLeft: 6 }} />
+                    </div>
+                </div>
             </div>
-            <button
-              style={{
-                fontSize: 14,
-                fontWeight: 600,
-                padding: "6px 16px",
-                marginTop: 24,
-                cursor: "pointer",
-              }}
-              onClick={onConnectWallet}
-            >
-              CONNECT WALLET
-            </button>
-          </div>
+            <div style={{ width: '30%', display: 'flex', flexDirection: 'column' }}>
+                <div style={{ padding: 16 }}>
+                    <p style={{ fontSize: 20, color: 'white' }}>{nftTitle}</p>
+                </div>
+                <div style={{ height: 1, background: '#444444' }}></div>
+                <div
+                    style={{
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                        padding: 16,
+                    }}
+                >
+                    <p style={{ fontSize: 14, color: '#9E9E9E' }}>{nftDescription}</p>
+                    <div>
+                        <div style={{ display: 'flex', flexDirection: 'row', color: 'white' }}>
+                            <div>
+                                <p>Price</p>
+                                <p style={{ fontWeight: 600 }}>{`${price} ETH`}</p>
+                            </div>
+                            <div style={{ marginLeft: 60 }}>
+                                <p>Mints Remaining</p>
+                                <p style={{ fontWeight: 600 }}>{mintsRemain}</p>
+                            </div>
+                        </div>
+                        <div style={{ marginTop: 24 }}>
+                            <button disabled={mintBtnDisabled} className="mint-nft_btn" onClick={onMintNft}>
+                                MINT NFT
+                            </button>
+                            <button className="connect-wallet_btn" onClick={onConnectWallet}>
+                                CONNECT WALLET
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{ width: '30%', display: 'flex', flexDirection: 'column', marginLeft: 48 }}>
+                <p style={{ fontSize: 12, fontWeight: 400, color: '#9E9E9E' }}>Question 1</p>
+                <p className="question"></p>
+                <p style={{ fontSize: 12, fontWeight: 400, color: '#9E9E9E' }}>Question 2</p>
+                <p className="question"></p>
+                <p style={{ fontSize: 12, fontWeight: 400, color: '#9E9E9E' }}>Question 3</p>
+                <p className="question question3"></p>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Widget;
